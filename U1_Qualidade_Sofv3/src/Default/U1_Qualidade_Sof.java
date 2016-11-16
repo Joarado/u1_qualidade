e08b7eb1099819e784eb141e47ff2e82de6376a7
Joarado <joarado@hotmail.com>
2016-11-16 12:14:57 -0200

    Add files via upload

---
+++ U1_Qualidade_Sofv3/src/Default/U1_Qualidade_Sof.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Default;
+
+import Interface.Menu_Principal;
+
+
+public final class U1_Qualidade_Sof {
+
+    public static int[] Totais = {};
+  
+            
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Menu_Principal().setVisible(true);
+        boolean receitas = false;
+       
+        
+    }
+    
+}
